75eb9f151237cf9330ccbb91f7be1c16fc18efbd
Philip Winther <mail@philip-winther.dk>
2021-03-12 12:08:55 +0100

    Add files via upload

---
+++ www/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Blog Welcome",
+        "excerpt":"This is the start of my blog   ","categories": ["Blog"],
+        "tags": [],
+        "url": "/blog/Blog-Welcome/",
+        "teaser": "/assets/images/pw-logo.png"
+      },{
+        "title": "Tutorial Welcome",
+        "excerpt":"This is the start of my Tutorials   ","categories": ["Tutorial"],
+        "tags": [],
+        "url": "/tutorial/Tutorial-welcome/",
+        "teaser": "/assets/images/pw-logo.png"
+      },]
